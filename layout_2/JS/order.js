a208ed4def0719cadef49e97290a0b75373a95ac
7XEZ <120032855+7XEZ@users.noreply.github.com>
2023-11-26 13:33:30 +0500

    Add files via upload

--- layout_2/JS/order.js
+++ layout_2/JS/order.js
@@ -1,12 +1,35 @@
 const subButton = document.querySelector('.button__form');
 const modal = document.querySelector('#modal');
 const closeBtn = document.querySelector('#close__btn');
-const myForm = document.querySelector('.form')
+const myForm = document.querySelector('.form');
 
-subButton.addEventListener('click', function(event){
+function validateForm(form) {
+    let valid = true;
+    ['name', 'phone', 'comment'].forEach(function(fieldName) {
+        const field = form.elements[fieldName];
+        if (!validateField(field)) {
+            valid = false;
+        }
+    });
+    return valid;
+}
+
+function validateField(field) {
+    let errorDiv = field.nextElementSibling;
+    if (!field.checkValidity()) {
+        errorDiv.textContent = field.validationMessage;
+        field.classList.add('error-highlight');
+        return false;
+    } else {
+        errorDiv.textContent = '';
+        field.classList.remove('error-highlight');
+        return true;
+    }
+}
+
+subButton.addEventListener('click', function(event) {
     event.preventDefault();
-
-    if(validateForm(myForm)){
+    if (validateForm(myForm)) {
         const data = {
             name: myForm.elements.name.value,
             phone: myForm.elements.phone.value,
@@ -14,39 +37,26 @@ subButton.addEventListener('click', function(event){
             to: myForm.elements.to.value
         };
 
-
-       const xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                modal.style.display = "flex";
+            } else {
+                console.error('Ошибка отправки: ', xhr.response);
+                // Здесь можно добавить обработку ошибок сервера, например, отобразить сообщение об ошибке
+            }
+        };
+        xhr.onerror = function() {
+            console.error('Ошибка запроса');
+            // Здесь можно добавить обработку ошибок сети, например, отобразить сообщение об ошибке
+        };
         xhr.send(JSON.stringify(data));
     }
+});
 
-    modal.style.display = "flex";
-
-    closeBtn.addEventListener('click', function(){
-       modal.style.display = 'none';
-    });
-
-    function validateForm(form){
-        let valid = true
-
-        if (!validateField(form.elements.name)){
-            valid = false;
-        }
-
-        if (!validateField(form.elements.phone)){
-            valid = false;
-        }
-
-        if (!validateField(form.elements.comment)){
-            valid = false;
-        }
-
-        return valid
-    }
-
-    function validateField(field){
-        field.nextElementSibling.textContent = field.validationMessage;
-
-        return field.checkValidity();
-    }
+closeBtn.addEventListener('click', function() {
+    modal.style.display = 'none';
 });
