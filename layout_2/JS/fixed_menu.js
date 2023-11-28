61e73f03cb41219630c37f070dce9ea059ffcbd1
7XEZ <120032855+7XEZ@users.noreply.github.com>
2023-11-28 18:28:36 +0500

    Add files via upload

---
+++ layout_2/JS/fixed_menu.js
@@ -0,0 +1,38 @@
+const section = $('section');
+const display = $('.maincontent');
+
+section.first().addClass('active');
+
+const performTransition = sectionEQ => {
+    const position = sectionEQ * -110;
+    
+    display.css({
+        transform: `translateY(${position}%)` // исправлено на обратные кавычки
+    });
+
+    section.eq(sectionEQ).addClass('active').siblings().removeClass('active');
+}
+
+const scrollViewport = direction => {
+    const activeSection = section.filter('.active');
+    const nextSection = activeSection.next('section');
+    const prevSection = activeSection.prev('section');
+
+    if (direction === "next" && nextSection.length) {
+        performTransition(nextSection.index('section'));
+    }
+
+    if (direction === "prev" && prevSection.length) {
+        performTransition(prevSection.index('section'));
+    }
+}
+
+$(window).on("wheel", e => {
+    const deltaY = e.originalEvent.deltaY;
+
+    if (deltaY > 0) {
+        scrollViewport("next");
+    } else if (deltaY < 0) {
+        scrollViewport("prev");
+    }
+});
