145fdb22e469d3cb8f0b0cce95073d502706ceb7
7XEZ <120032855+7XEZ@users.noreply.github.com>
2023-12-02 13:41:43 +0500

    Add files via upload

--- layout_2/JS/color_menu.js
+++ layout_2/JS/color_menu.js
@@ -1,18 +1,38 @@
 $(document).ready(function(){
+    var $window = $(window);
+
+    function adjustMenuWidth(open) {
+        var windowsize = $window.width();
+        if (windowsize <= 768 && open) {
+            // Если ширина окна 768px или меньше и блок открывается
+            $('.color__menu-conteiner--active').css('width', '100%');
+        } else {
+            // В противном случае сбрасываем инлайновый стиль ширины
+            $('.color__menu-conteiner').css('width', '');
+        }
+    }
+
+    // Выполнить при первой загрузке страницы
+    adjustMenuWidth(false);
+
+    // Выполнить при каждом изменении размера окна
+    $(window).resize(function() {
+        adjustMenuWidth($('.color__menu-conteiner--active').length > 0);
+    });
+
     $('.color__menu-item').on('click', function(){
-        // Определяем контейнер, связанный с нажатым элементом
         var content = $(this).find('.color__menu-conteiner');
 
-        // Проверяем, активен ли уже этот блок
+        // Проверяем, активен ли уже этот контейнер
         if(content.hasClass('color__menu-conteiner--active')) {
-            // Если блок уже активен, то убираем классы, закрывая его
+            // Если активен, удаляем класс, закрывая его
             content.removeClass('color__menu-conteiner--active');
+            adjustMenuWidth(false);
         } else {
-            // Если блок не активен, убираем класс 'active' у всех блоков
-            $('.color__menu-conteiner').removeClass('color__menu-conteiner--active');
-            
-            // И добавляем класс 'active' только к этому блоку
+            // Если нет, закрываем все другие, удаляем инлайновые стили и открываем этот
+            $('.color__menu-conteiner').removeClass('color__menu-conteiner--active').css('width', '');
             content.addClass('color__menu-conteiner--active');
+            adjustMenuWidth(true);
         }
     });
 });
